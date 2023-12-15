810a0c154864339a4d35965d011e2ecaa364db0b
DivineDK <divinedorvlo@yahoo.com>
2023-12-15 06:33:20 -0800

    5

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/DivineDK/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib.so
-export LD_PRELOAD="$PWD/../lib.so"
+wget -0 /tmp/lib.so https://github.com/DivineDK/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib.so
+export LD_PRELOAD=/tmp/lib.so
